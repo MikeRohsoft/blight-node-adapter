fc080e51263e24c789cf5de3b93e6cbb16a2fcd8
Mike Rohsoft <godlike@cracercrew.de>
2019-06-29 01:51:26 +0200

    fixes

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-    {
-        username: 'Scriptkiddy1337',
-        password: 'Tele48195',
-        maxRepeats: -1, // -1 = infinity
-        timeOut: 10 * 1000, // wait 7 seconds after each request
-    },
-];
